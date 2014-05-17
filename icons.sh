75245988cd4375c064290c6a24780bd23b99556d
Michael Mackenzie <vebbed@gmail.com>
2014-05-17 20:49:06 +1200

    first commit
    make sure this file is executable

---
+++ icons.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Author: Mike Mackenzie
+# Hides or shows all desktop icons useful if you want an unclutered desktop on
+# OS X. People do say that it also helps speed up the computer as he sytsem
+# isn't generating thumbnails all the time. Whatever, you know the Internet...
+# lies, lies everywhere! 
+
+if [ $1 == 'show' ]; then
+    defaults write com.apple.finder CreateDesktop -bool true
+    killall Finder
+fi
+
+if [ $1 == 'hide' ]; then
+    defaults write com.apple.finder CreateDesktop -bool false
+    killall Finder
+fi
+
