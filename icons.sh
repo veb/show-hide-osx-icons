302b6b58f8a6acee90e53062b7431b72f80a0e88
Mike Mackenzie <veb@users.noreply.github.com>
2015-09-05 14:47:14 +1200

    Update icons.sh
    Typos

--- icons.sh
+++ icons.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Author: Mike Mackenzie
-# Hides or shows all desktop icons useful if you want an unclutered desktop on
-# OS X. People do say that it also helps speed up the computer as the sytsem
+# Hides or shows all desktop icons useful if you want an uncluttered desktop on
+# OS X. People do say that it also helps speed up the computer as the system
 # isn't generating thumbnails all the time. Whatever, you know the Internet...
 # lies, lies everywhere! 
 
